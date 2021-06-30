e651161e3459e529eef52ad54b45020f446fd392
Foteini Lempesi <flebesi@hotmail.gr>
2021-06-30 14:41:26 +0300

    R EmployeeViewForm

--- FuelStationProject/FuelStationProject/WUI/EmployeeViewForm.cs
+++ FuelStationProject/FuelStationProject/WUI/EmployeeViewForm.cs
@@ -13,21 +13,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FuelStationProject.WUI
-{
-    public partial class EmployeeViewForm : DevExpress.XtraEditors.XtraForm
-    {
+namespace FuelStationProject.WUI {
+    public partial class EmployeeViewForm : DevExpress.XtraEditors.XtraForm {
         public DataSet ViewData { get; set; }
-
         public DatabaseConnectionController DBController { get; set; }
-
-        public EmployeeViewForm()
-        {
+        public EmployeeViewForm() {
             InitializeComponent();
         }
 
-        private void EmployeeViewForm_Load(object sender, EventArgs e)
-        {
+        private void EmployeeViewForm_Load(object sender, EventArgs e) {
             gridView1.OptionsView.NewItemRowPosition = NewItemRowPosition.Top;
 
             CultureInfo.CurrentCulture = new CultureInfo("en-US", false);
@@ -35,26 +29,20 @@ namespace FuelStationProject.WUI
 
             RefreshEmployeeGrid();
         }
-        private void repSaveButtonEdit_Click(object sender, EventArgs e)
-        {
+        private void repSaveButtonEdit_Click(object sender, EventArgs e) {
             SaveButtonCode();
 
         }
 
-        private void repdeleteButtonEdit_Click(object sender, EventArgs e)
-        {
+        private void repdeleteButtonEdit_Click(object sender, EventArgs e) {
             DeletebuttonCode();
 
         }
 
-
-
-        private void SaveButtonCode()
-        {
+        private void SaveButtonCode() {
             DialogResult result = MessageBox.Show("Are you sure you want to Change this entry ?", "Warning", MessageBoxButtons.OKCancel);
 
-            if (result == DialogResult.OK)
-            {
+            if (result == DialogResult.OK) {
 
                 SaveEmployee();
                 RefreshEmployeeGrid();
@@ -62,14 +50,10 @@ namespace FuelStationProject.WUI
             }
         }
 
-
-        private void DeletebuttonCode()
-        {
+        private void DeletebuttonCode() {
             DialogResult result = MessageBox.Show("Are you sure you want to delete this entry ?", "Warning", MessageBoxButtons.OKCancel);
 
-            if (result == DialogResult.OK)
-            {
-
+            if (result == DialogResult.OK) {
 
                 SqlCommand command = new SqlCommand(string.Format(Resources.DeleteEmployee, Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "ID"))), DBController._SqlConnection);
 
@@ -79,62 +63,53 @@ namespace FuelStationProject.WUI
             }
         }
 
-        private void RefreshEmployeeGrid()
-        {
+        private void RefreshEmployeeGrid() {
             ViewData = new DataSet();
             SqlDataAdapter adapter = new SqlDataAdapter(Resources.SelectEmployeeTable, DBController._SqlConnection);
             int response = adapter.Fill(ViewData);
-
             gridView1.OptionsView.ShowGroupPanel = false;
             gridEmployee.DataSource = ViewData.Tables[0];
-
         }
 
-        public void SaveEmployee()
-        {
+        public void SaveEmployee() {
             string id = Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "ID"));
-            // Guid id = Guid.Parse(Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "ID")));
             string name = Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Name"));
             string surname = Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Surname"));
             DateTime dateStart;
             DateTime date;
             DateTime? dateEnd = null;
             decimal salary;
-            if (gridColumnDateEnd != null)
-            {
+            if (gridColumnDateEnd != null) {
                 dateEnd = DateTime.TryParse(Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "DateEnd")), out date) ? date : (DateTime?)null;
             }
 
             if (!string.IsNullOrWhiteSpace(name) && !string.IsNullOrWhiteSpace(surname) &&
                   decimal.TryParse(Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Salary")).Replace(',', '.'), out salary) && salary > 0
-                  && DateTime.TryParse(Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "DateStart")), out dateStart))
-            {
-
-                if (!string.IsNullOrWhiteSpace(id)) {
-                    SqlCommand command = new SqlCommand(string.Format(Resources.UpdateEmployee, name, surname, dateStart, dateEnd, salary, id), DBController._SqlConnection);
-                    int rowsAffected = command.ExecuteNonQuery();
-                }
-                else {
-                    SqlCommand command = new SqlCommand(string.Format(Resources.InsertEmployee, name, surname, dateStart, dateEnd, salary), DBController._SqlConnection);
-                    int rowsAffected = command.ExecuteNonQuery();
-                }
-
+                  && DateTime.TryParse(Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "DateStart")), out dateStart)) {
+                SaveToDB(id, name, surname, dateStart, dateEnd, salary);
 
             }
-            else
-            {
+            else {
 
                 MessageBox.Show("Please fill all fields with valid values!");
             }
         }
 
-        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
-        {
+        private void SaveToDB(string id, string name, string surname, DateTime dateStart, DateTime? dateEnd, decimal salary) {
+            if (!string.IsNullOrWhiteSpace(id)) {
+                SqlCommand command = new SqlCommand(string.Format(Resources.UpdateEmployee, name, surname, dateStart, dateEnd, salary, id), DBController._SqlConnection);
+                int rowsAffected = command.ExecuteNonQuery();
+            }
+            else {
+                SqlCommand command = new SqlCommand(string.Format(Resources.InsertEmployee, name, surname, dateStart, dateEnd, salary), DBController._SqlConnection);
+                int rowsAffected = command.ExecuteNonQuery();
+            }
+        }
+
+        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
             RefreshEmployeeGrid();
         }
 
-        private void gridEmployee_Click(object sender, EventArgs e) {
 
-        }
     }
 }
