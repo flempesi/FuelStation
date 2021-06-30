7f65bb30374a879fa8d77f11b32f75f419034b26
Foteini Lempesi <flebesi@hotmail.gr>
2021-06-30 14:37:34 +0300

    ItemViewForm Refact

--- FuelStationProject/FuelStationProject/WUI/ItemViewForm.cs
+++ FuelStationProject/FuelStationProject/WUI/ItemViewForm.cs
@@ -16,59 +16,52 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FuelStationProject.WUI
-{
-    public partial class ItemViewForm : DevExpress.XtraEditors.XtraForm
-    {
+namespace FuelStationProject.WUI {
+    public partial class ItemViewForm : DevExpress.XtraEditors.XtraForm {
         public DataSet ViewData { get; set; }
 
         public DatabaseConnectionController DBController { get; set; }
 
-        public ItemViewForm()
-        {
+        public ItemViewForm() {
             InitializeComponent();
         }
 
 
-        private void ItemViewForm_Load(object sender, EventArgs e)
-        {
+        private void ItemViewForm_Load(object sender, EventArgs e) {
+            ItemViewFormLoader();
+
+        }
+        private void repSaveButtonEdit_Click(object sender, EventArgs e) {
+            SaveButtonCode();
+
+        }
+
+        private void repdeleteButtonEdit_Click(object sender, EventArgs e) {
+            DeletebuttonCode();
+
+        }
+        private void barButtonItem3_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
+            RefreshItemGrid();
+        }
+        private void ItemViewFormLoader() {
             gridView1.OptionsView.NewItemRowPosition = NewItemRowPosition.Top;
             CultureInfo.CurrentCulture = new CultureInfo("en-US", false);
             CultureInfo.CurrentUICulture = new CultureInfo("en-US", false);
 
             RefreshItemGrid();
-            foreach (ItemTypeCategory type in Enum.GetValues(typeof(ItemTypeCategory)))
-            {
+            foreach (ItemTypeCategory type in Enum.GetValues(typeof(ItemTypeCategory))) {
                 cmbItemType.Items.Add(type);
             }
-
-        }
-
-
-        private void repSaveButtonEdit_Click(object sender, EventArgs e)
-        {
-            SaveButtonCode();
-
-        }
-
-        private void repdeleteButtonEdit_Click(object sender, EventArgs e)
-        {
-            DeletebuttonCode();
-
-        }
-        private void barButtonItem3_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
-        {
-            RefreshItemGrid();
         }
 
 
 
-        private void SaveButtonCode()
-        {
+
+
+        private void SaveButtonCode() {
             DialogResult result = MessageBox.Show("Are you sure you want to Change this entry ?", "Warning", MessageBoxButtons.OKCancel);
 
-            if (result == DialogResult.OK)
-            {
+            if (result == DialogResult.OK) {
 
                 UpdateItem();
                 RefreshItemGrid();
@@ -77,12 +70,10 @@ namespace FuelStationProject.WUI
         }
 
 
-        private void DeletebuttonCode()
-        {
+        private void DeletebuttonCode() {
             DialogResult result = MessageBox.Show("Are you sure you want to delete this entry ?", "Warning", MessageBoxButtons.OKCancel);
 
-            if (result == DialogResult.OK)
-            {
+            if (result == DialogResult.OK) {
 
 
                 SqlCommand command = new SqlCommand(string.Format(Resources.DeleteItem, Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "ID"))), DBController._SqlConnection);
@@ -93,8 +84,7 @@ namespace FuelStationProject.WUI
             }
         }
 
-        private void RefreshItemGrid()
-        {
+        private void RefreshItemGrid() {
             ViewData = new DataSet();
             SqlDataAdapter adapter = new SqlDataAdapter(Resources.SelectItemTable, DBController._SqlConnection);
             int response = adapter.Fill(ViewData);
@@ -104,10 +94,8 @@ namespace FuelStationProject.WUI
 
         }
 
-        public void UpdateItem()
-        {
+        public void UpdateItem() {
             string id = Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "ID"));
-            //Guid id = Guid.Parse(Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "ID")));
             string code = Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Code"));
             string description = Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Description"));
             string itemType = Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "ItemType"));
@@ -117,24 +105,27 @@ namespace FuelStationProject.WUI
             if (!string.IsNullOrWhiteSpace(code) && !string.IsNullOrWhiteSpace(description) && !string.IsNullOrWhiteSpace(itemType)
                 &&
                   decimal.TryParse(Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Price")), out price) && price > 0
-                  && decimal.TryParse(Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Cost")), out cost) && cost > 0)
-            {
-                if (!string.IsNullOrWhiteSpace(id)) {
-
-                    SqlCommand command = new SqlCommand(string.Format(Resources.UpdateItem, code, description, itemType, price, cost, id), DBController._SqlConnection);
-                    int rowsAffected = command.ExecuteNonQuery();
-
-                }
-                else {
-                    SqlCommand command = new SqlCommand(string.Format(Resources.InsertItem, code, description, itemType, price, cost), DBController._SqlConnection);
-                    int rowsAffected = command.ExecuteNonQuery();
-                }
+                  && decimal.TryParse(Convert.ToString(gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Cost")), out cost) && cost > 0) {
+                SaveToDB(id, code, description, itemType, price, cost);
+                RefreshItemGrid();
 
             }
-            else
-            {
+            else {
 
                 MessageBox.Show("Please fill all fields with valid values!");
+            }
+        }
+
+        private void SaveToDB(string id, string code, string description, string itemType, decimal price, decimal cost) {
+            if (!string.IsNullOrWhiteSpace(id)) {
+
+                SqlCommand command = new SqlCommand(string.Format(Resources.UpdateItem, code, description, itemType, price, cost, id), DBController._SqlConnection);
+                int rowsAffected = command.ExecuteNonQuery();
+
+            }
+            else {
+                SqlCommand command = new SqlCommand(string.Format(Resources.InsertItem, code, description, itemType, price, cost), DBController._SqlConnection);
+                int rowsAffected = command.ExecuteNonQuery();
             }
         }
 
